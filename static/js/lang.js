487e78737ba8a4aa6123117d517c4dd75fcf21c5
houyukun <601432505@qq.com>
2022-03-08 18:08:25 +0800

    first commit

---
+++ static/js/lang.js
@@ -0,0 +1,15 @@
+import common from '../../common/commonLang.js'
+export default {	
+	cn: {
+		common: common.cn,
+		langList: ['中文版', '英文版'],
+		team: '团队列表',
+		share: '分享'
+	},
+	us: {
+		common: common.us,
+		langList : ['中文版', '英文版'],
+		team: 'Team List',		
+		share: 'Share'
+	}
+}
